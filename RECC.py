709b4963892c1843bbba1c76015a68517acd57e1
Martijn <martijnbos1996@xs4all.nl>
2019-08-09 14:48:12 +0200

    2D Outliers

--- RECC.py
+++ RECC.py
@@ -159,10 +159,14 @@ def patch_match(i, edges, gt, fact_x, fact_y, x_b, y_b, mask, edges_0, gt_0, fac
     origin_y   = np.zeros(ppp)
     target_lon = np.zeros(ppp)
     target_lat = np.zeros(ppp)
+    o_x        = np.zeros(ppp)
+    o_y        = np.zeros(ppp)
+    t_x        = np.zeros(ppp)
+    t_y        = np.zeros(ppp)
     j=-1
     it = 0
     if it_cancel == 1:
-        while j <= ppp-2 and it <= it_max*ppp:
+        while j <= ppp-2 and it*it_cancel <= it_max*ppp:
             x_i_0 = randint(w,x_b_0-w)
             y_i_0 = randint(w,y_b_0-w)
             check1 = mask_0_c[x_i_0,y_i_0]
@@ -200,6 +204,10 @@ def patch_match(i, edges, gt, fact_x, fact_y, x_b, y_b, mask, edges_0, gt_0, fac
                         origin_y[j]   = y_i*fact_y
                         target_lon[j] = lon_0
                         target_lat[j] = lat_0
+                        o_x[j] = x_i
+                        o_y[j] = y_i
+                        t_x[j] = x_i_0
+                        t_y[j] = y_i_0
                     else:
                         print("["+"{:.0f}".format(((3+j)+(i-1)*(ppp+3))/steps)+"%] ("+"{:.0f}".format(cv_score)+","+"{:.1f}".format(dst)+") Match failed.")
                 else:
@@ -211,53 +219,17 @@ def patch_match(i, edges, gt, fact_x, fact_y, x_b, y_b, mask, edges_0, gt_0, fac
         origin_y = origin_y[origin_y!=0]
         target_lon = target_lon[target_lon!=0]
         target_lat = target_lat[target_lat!=0]
-    if it_cancel == 0:
-        while j <= ppp-2:
-            x_i_0 = randint(w,x_b_0-w)
-            y_i_0 = randint(w,y_b_0-w)
-            check1 = mask_0_c[x_i_0,y_i_0]
-            check2 = sumedges_0[x_i_0,y_i_0]
-            if check1 <= 0 and check2 >= minfeatures:
-                it = it+1
-                target = edges_0[x_i_0-w:x_i_0+w,y_i_0-w:y_i_0+w]
-                sum_target = np.sum(target)
-                RECC_s = np.zeros(edges.shape)
-                for x in range(max(w,x_i_0-4*w),min(x_b-w,x_i_0+4*w)):
-                    for y in range(max(w,y_i_0-4*w),min(y_b-w,y_i_0+4*w)):
-                        patch = edges[x-w:x+w,y-w:y+w]
-                        RECC_s[x,y]=np.sum(np.multiply(target,patch))/(sum_target+np.sum(patch))           
-                max_one  = np.partition(RECC_s.flatten(),-1)[-1]
-                max_n    = np.partition(RECC_s.flatten(),-4-1)[-4-1]
-                x_i    = np.where(RECC_s >= max_one)[1][0]
-                y_i    = np.where(RECC_s >= max_one)[0][0]
-                x_n      = np.where(RECC_s >= max_n)[1][0:-1]
-                y_n      = np.where(RECC_s >= max_n)[0][0:-1]
-                cv_score = sum(np.sqrt(np.square(x_i-x_n)+np.square(y_i-y_n)))          
-                if cv_score <= cv_max:
-                    lon = gt[0] + gt[1]*x_i*fact_x + gt[2]*y_i*fact_y
-                    lat = gt[3] + gt[4]*x_i*fact_x + gt[5]*y_i*fact_y
-                    lon_0 = gt_0[0] + gt_0[1]*y_i_0*fact_x_0 + gt_0[2]*x_i_0*fact_y_0
-                    lat_0 = gt_0[3] + gt_0[4]*y_i_0*fact_x_0 + gt_0[5]*x_i_0*fact_y_0
-                    dst = calc_distance(lat,lon,lat_0,lon_0)
-                    if dst <= dst_max:
-                        j=j+1
-                        print("["+"{:.0f}".format(((3+j)+(i-1)*(ppp+3))/steps)+"%] ("+"{:.0f}".format(cv_score)+","+"{:.1f}".format(dst)+") Succesful patch-match nr "+str(j+1)+" of "+str(ppp)+".")
-                        mask_0_c[x_i_0-v:x_i_0+v,y_i_0-v:y_i_0+v]=1
-                        dist[j]       = dst
-                        dist_lon[j]   = lon_0-lon
-                        dist_lat[j]   = lat_0-lat
-                        origin_x[j]   = x_i*fact_x
-                        origin_y[j]   = y_i*fact_y
-                        target_lon[j] = lon_0
-                        target_lat[j] = lat_0
-                    else:
-                        print("["+"{:.0f}".format(((3+j)+(i-1)*(ppp+3))/steps)+"%] ("+"{:.0f}".format(cv_score)+","+"{:.1f}".format(dst)+") Match failed.")
-                else:
-                    print("["+"{:.0f}".format(((3+j)+(i-1)*(ppp+3))/steps)+"%] ("+"{:.0f}".format(cv_score)+",-) Match failed.")
-                    
-    return dist, dist_lon, dist_lat, origin_x, origin_y, target_lon, target_lat
+        o_x = o_x[o_x!=0]
+        o_y = o_y[o_y!=0]
+        t_x = t_x[t_x!=0]
+        t_y = t_y[t_y!=0] 
+        # flip the target x and y for some reason:
+        t_x_temp = t_y
+        t_y = t_x
+        t_x = t_x_temp
+    return dist, dist_lon, dist_lat, origin_x, origin_y, target_lon, target_lat, o_x, o_y, t_x, t_y
 
-def remove_outliers(i, ppp, steps, outlier_type, dist, dist_lon, dist_lat, origin_x, origin_y, target_lon, target_lat):
+def remove_outliers(i, ppp, conf, steps, outlier_type, dist, dist_lon, dist_lat, origin_x, origin_y, target_lon, target_lat, o_x, o_y, t_x, t_y):
     gcplist = " "
     size = len(dist)
     if outlier_type == 0:
@@ -279,7 +251,7 @@ def remove_outliers(i, ppp, steps, outlier_type, dist, dist_lon, dist_lat, origi
             for j in range(len(fliers_lat)):
                 flier_indices_lat[j] = np.where(dist_lat==fliers_lat[j])[0][0]
             flier_indices = np.union1d(flier_indices_lon,flier_indices_lat)
-            flier_indices = (np.union1d(flier_indices,flier_indices_dst)).astype(int)
+            flier_indices = (np.union1d(flier_indices,flier_indices_dst)).astype(int)  
             dist       = np.delete(dist,flier_indices)
             dist_lon   = np.delete(dist_lon,flier_indices)
             dist_lat   = np.delete(dist_lat,flier_indices)
@@ -287,25 +259,41 @@ def remove_outliers(i, ppp, steps, outlier_type, dist, dist_lon, dist_lat, origi
             origin_y   = np.delete(origin_y,flier_indices)
             target_lon = np.delete(target_lon,flier_indices)
             target_lat = np.delete(target_lat,flier_indices) 
-    elif outlier_type == 1:        # OUTDATED:
-        mean = np.mean(dist)
-        std = np.std(dist)
-        bol1 = np.linspace(0,len(dist)-1,len(dist))
-        bol2 = np.zeros(dist.shape)
-        bol2[dist<=mean-2.58*(std/sqrt(len(dist)))]=1
-        bol2[dist>=mean+2.58*(std/sqrt(len(dist)))]=1
-        bol = (bol1*bol2).astype(int)
-        dist = np.delete(dist,bol)
-        dist_lon   = np.delete(dist_lon,bol)
-        dist_lat   = np.delete(dist_lat,bol)
-        origin_x   = np.delete(origin_x,bol)
-        origin_y   = np.delete(origin_y,bol)
-        target_lon = np.delete(target_lon,bol)
-        target_lat = np.delete(target_lat,bol)
+            o_x        = np.delete(o_x,flier_indices)
+            o_y        = np.delete(o_y,flier_indices)
+            t_x        = np.delete(t_x,flier_indices)
+            t_y        = np.delete(t_y,flier_indices)
+    elif outlier_type == 1:
+        if   conf == 95:
+            s = 5.991
+        elif conf == 90:
+            s = 4.605
+        elif conf == 80:
+            s = 3.219
+        elif conf == 75:
+            s = 2.770
+        elif conf == 50:
+            s = 1.388
+        d_x = t_x - o_x
+        d_y = t_y - o_y
+        d_x_m = d_x - np.median(d_x)
+        d_y_m = d_y - np.median(d_y)        
+        indices = ((d_x_m/sqrt(np.var(d_x_m)))**2 + (d_y_m/sqrt(np.var(d_y_m)))**2 >= s)    
+        dist       = dist[~indices]
+        dist_lon   = dist_lon[~indices]
+        dist_lat   = dist_lat[~indices]
+        origin_x   = origin_x[~indices]
+        origin_y   = origin_y[~indices]
+        target_lon = target_lon[~indices]
+        target_lat = target_lat[~indices]
+        o_x        = o_x[~indices]
+        o_y        = o_y[~indices]
+        t_x        = t_x[~indices]
+        t_y        = t_y[~indices]
     print("["+"{:.0f}".format(((2+ppp)+(i-1)*(ppp+3))/steps)+"%] Removed "+str(size-len(dist))+" outliers.")      
     for k in range(len(origin_x)):
         gcplist = gcplist+"-gcp "+str(origin_x[k])+" "+str(origin_y[k])+" "+str(target_lon[k])+" "+str(target_lat[k])+" "        
-    return gcplist, dist, dist_lon, dist_lat, origin_x, origin_y, target_lon, target_lat
+    return gcplist, dist, dist_lon, dist_lat, origin_x, origin_y, target_lon, target_lat, o_x, o_y, t_x, t_y
       
 def georeference(i,wdir,ppp,path,file,steps,gcplist):
     print("["+"{:.0f}".format(((3+ppp)+(i-1)*(ppp+3))/steps)+"%] Georeferencing image nr "+str(i)+".")
@@ -318,3 +306,4 @@ def georeference(i,wdir,ppp,path,file,steps,gcplist):
     os.system("gdal_translate -a_srs EPSG:4326 -of GTiff"+gcplist+"\""+path+"\" \""+path1+"\"")
     print("["+"{:.0f}".format(((3+ppp)+(i-1)*(ppp+3))/steps)+"%] Succesful translate, warping...")
     os.system("gdalwarp -r cubicspline -tps -co COMPRESS=NONE \""+path1+"\" \""+path2+"\"")    
+    
--- RECC_controler.py
+++ RECC_controler.py
@@ -3,7 +3,8 @@ wdir = r"E:"
 files = ["0","1"]
 
 file_type      = 0         # [0: Orthomosaic] [1: DEM]
-outlier_type   = 0         # [0: Boxplot] [1: 99%]
+outlier_type   = 1         # [0: Boxplot] [1: 2D Confidence]
+conf           = 75        # {50,75,80,90,95}
 gamma_correct  = 1         # {0,1}
 extra_blur     = 1         # {0,1}
 luma           = 709       # {240,601,709}
@@ -15,7 +16,7 @@ w              = 30        # Search square will have a length of {2*<...>*pixel_
 v              = 30        # Masking square will have a length of {2*<...> pixel_size} [m].
 s              = 4         # RECC search range distance is capped at {<...>*w*pixel_size} [m].
 it_cancel      = 1         # {0,1} 
-it_max         = 2         # Max number of iterations per match
+it_max         = 5         # Max number of iterations per match
 
 import RECC
 
@@ -24,52 +25,50 @@ path,steps = RECC.initialize(wdir,files,ppp)
 for i in range(len(path)):
     exec("edges_"+str(i)+", gt_"+str(i)+", fact_x_"+str(i)+", fact_y_"+str(i)+", x_b_"+str(i)+", y_b_"+str(i)+", mask_"+str(i)+" = RECC.file_to_edges(i,file_type,path[i],luma,gamma_correct,pixel_size,extra_blur,ppp,steps)")
     if i >= 1:
-        exec("dist_"+str(i)+", dist_lon_"+str(i)+", dist_lat_"+str(i)+", origin_x_"+str(i)+", origin_y_"+str(i)+", target_lon_"+str(i)+", target_lat_"+str(i)+" = RECC.patch_match(i, edges_"+str(i)+", gt_"+str(i)+", fact_x_"+str(i)+", fact_y_"+str(i)+", x_b_"+str(i)+", y_b_"+str(i)+", mask_"+str(i)+", edges_0, gt_0, fact_x_0, fact_y_0, x_b_0, y_b_0, mask_0, ppp, cv_max, dst_max, w, v, steps, s, it_cancel, it_max)")
-        exec("gcplist, dist_a_"+str(i)+", dist_lon_a_"+str(i)+", dist_lat_a_"+str(i)+", origin_x_a_"+str(i)+", origin_y_a_"+str(i)+", target_lon_a_"+str(i)+", target_lat_a_"+str(i)+" = RECC.remove_outliers(i, ppp, steps, outlier_type, dist_"+str(i)+", dist_lon_"+str(i)+", dist_lat_"+str(i)+", origin_x_"+str(i)+", origin_y_"+str(i)+", target_lon_"+str(i)+", target_lat_"+str(i)+")")
-        exec("RECC.georeference(i,wdir,ppp,path[i],files[i],steps,gcplist)")
+        exec("dist_"+str(i)+", dist_lon_"+str(i)+", dist_lat_"+str(i)+", origin_x_"+str(i)+", origin_y_"+str(i)+", target_lon_"+str(i)+", target_lat_"+str(i)+", o_x_"+str(i)+", o_y_"+str(i)+", t_x_"+str(i)+", t_y_"+str(i)+" = RECC.patch_match(i, edges_"+str(i)+", gt_"+str(i)+", fact_x_"+str(i)+", fact_y_"+str(i)+", x_b_"+str(i)+", y_b_"+str(i)+", mask_"+str(i)+", edges_0, gt_0, fact_x_0, fact_y_0, x_b_0, y_b_0, mask_0, ppp, cv_max, dst_max, w, v, steps, s, it_cancel, it_max)")
+        exec("gcplist, dist_a_"+str(i)+", dist_lon_a_"+str(i)+", dist_lat_a_"+str(i)+", origin_x_a_"+str(i)+", origin_y_a_"+str(i)+", target_lon_a_"+str(i)+", target_lat_a_"+str(i)+",o_x_a_"+str(i)+", o_y_a_"+str(i)+", t_x_a_"+str(i)+", t_y_a_"+str(i)+" = RECC.remove_outliers(i, ppp, conf, steps, outlier_type, dist_"+str(i)+", dist_lon_"+str(i)+", dist_lat_"+str(i)+", origin_x_"+str(i)+", origin_y_"+str(i)+", target_lon_"+str(i)+", target_lat_"+str(i)+",o_x_"+str(i)+", o_y_"+str(i)+", t_x_"+str(i)+", t_y_"+str(i)+")")
+        #exec("RECC.georeference(i,wdir,ppp,path[i],files[i],steps,gcplist)")
 print("[100%] Done.")        
 
 #%%
-import numpy as np
-import matplotlib.pyplot as plt
-clist = list(np.random.choice(range(256), size=len(dist_1)))
-clist_a = list(np.random.choice(range(256), size=len(dist_a_1)))
+#import numpy as np
+#import matplotlib.pyplot as plt
+#clist = list(np.random.choice(range(256), size=len(dist_1)))
+#clist_a = list(np.random.choice(range(256), size=len(dist_a_1)))
 
 #%% TARGET  
-plt.imshow(edges_0, cmap='gray', interpolation='nearest')
-plt.scatter((target_lon_1-gt_1[0])/(gt_1[1]*fact_x_1), (target_lat_1-gt_1[3])/(gt_1[5]*fact_y_1),c=clist)
+#plt.imshow(edges_0, cmap='gray', interpolation='nearest')
+#plt.scatter((target_lon_1-gt_1[0])/(gt_1[1]*fact_x_1), (target_lat_1-gt_1[3])/(gt_1[5]*fact_y_1),c=clist)
 
-#%% ORIGIN IN IMAGE
-plt.imshow(edges_1, cmap='gray', interpolation='nearest')
-plt.scatter(origin_x_1/fact_x_1, origin_y_1/fact_y_1,c=clist)
+#%% ORIGIN
+#plt.imshow(edges_1, cmap='gray', interpolation='nearest')
+#plt.scatter(origin_x_1/fact_x_1, origin_y_1/fact_y_1,c=clist)
 
 #%% DOUBLE SCATTER
-plt.scatter((target_lon_1-gt_1[0])/(gt_1[1]*fact_x_1), (target_lat_1-gt_1[3])/(gt_1[5]*fact_y_1),c=clist)
-plt.scatter(origin_x_1/fact_x_1, origin_y_1/fact_y_1,c=clist)
-
-#%% DISTANCE BOXPLOT
-plt.boxplot(dist_lat_1)
+#plt.scatter(o_x_1,o_y_1,c=clist)
+#plt.scatter(t_x_1,t_y_1,c=clist)
 
 #%% (INLIERS) TARGET
-plt.imshow(edges_0, cmap='gray', interpolation='nearest')
-plt.scatter((target_lon_a_1-gt_1[0])/(gt_1[1]*fact_x_1), (target_lat_a_1-gt_1[3])/(gt_1[5]*fact_y_1),c=clist_a)
+#plt.imshow(edges_0, cmap='gray', interpolation='nearest')
+#plt.scatter((target_lon_a_1-gt_1[0])/(gt_1[1]*fact_x_1), (target_lat_a_1-gt_1[3])/(gt_1[5]*fact_y_1),c=clist_a)
 
-#%% (INLIERS) ORIGIN IN IMAGE
-plt.imshow(edges_1, cmap='gray', interpolation='nearest')
-plt.scatter(origin_x_a_1/fact_x_1, origin_y_a_1/fact_y_1,c=clist_a)
+#%% (INLIERS) ORIGIN
+#plt.imshow(edges_1, cmap='gray', interpolation='nearest')
+#plt.scatter(origin_x_a_1/fact_x_1, origin_y_a_1/fact_y_1,c=clist_a)
 
-#%% (INTLIERS) DISTANCE BOXPLOT
-plt.boxplot(dist_a_1)
+#%% (INLIERS) DOUBLE SCATTER
+#plt.scatter(o_x_a_1,o_y_a_1,c=clist_a)
+#plt.scatter(t_x_a_1,t_y_a_1,c=clist_a)
 
 #%% STORE WORKSPACE
-#import shelve
-#my_shelf = shelve.open(wdir+"\\shelf.out",'n') # 'n' for new
-#for key in dir():
-#    try:
-#        my_shelf[key] = globals()[key]
-#    except:
-#        print('ERROR shelving: {0}'.format(key))
-#my_shelf.close()
+import shelve
+my_shelf = shelve.open(wdir+"\\shelf.out",'n') # 'n' for new
+for key in dir():
+    try:
+        my_shelf[key] = globals()[key]
+    except:
+        print('ERROR shelving: {0}'.format(key))
+my_shelf.close()
 
 #%% LOAD WORKSPACE
 #import shelve
