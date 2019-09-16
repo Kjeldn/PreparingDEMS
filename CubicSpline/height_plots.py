6f458c31405ea6e0203d547e452b0f32f8246673
wroel <wytze.roel@gmail.com>
2019-09-16 12:23:29 +0200

    Add height plot for beds

--- CubicSpline/height_plots.py
+++ CubicSpline/height_plots.py
@@ -3,6 +3,7 @@ import util
 import numpy as np
 import fiona
 import matplotlib.pyplot as plt
+import divide_into_beds as dib
 
 wd = ""
 paths = ["c01_verdonk-Rijweg stalling 1-201907091137_DEM-GR_cubic", 
@@ -34,5 +35,20 @@ heights = np.array((len(plants), len(paths)))
 for i in range(len(plants)):
     heights[i,:] = np.array([plane.getMaxValueAt(plants[i][1], plants[i][0]) for plane in planes])
     
-plt.plot(np.arange(len(plants)), [np.mean(heights[:,i]) for i in range(len(plants))])
-plt.fill_between(np.arange(len(plants)), [np.mean(heights[:,i]) - 2*np.std(heights[:,i]) for i in range(len(plants))], [np.mean(heights[:,i]) + 2*np.std(heights[:,i]) for i in range(len(plants))])
+# =============================================================================
+# plt.plot(np.arange(len(planes)), [np.median(heights[:,i]) for i in range(len(planes))])
+# plt.fill_between(np.arange(len(plants)), [np.percentile(heights[:,i], 25) for i in range(len(planes))], [np.percentile(heights[:,i], 75) for i in range(len(planes))])
+# plt.show()
+# =============================================================================
+
+beds = dib.divide(plants)
+heights_beds = []
+for bed in beds:
+    heights_array = np.array((len(bed), len(paths)))
+    for i in range(len(bed)):
+        heights[i,:] = np.array([plane.getMaxValueAt(bed[i][1], bed[i][0]) for plane in planes])
+    heights_beds.append(heights_array)
+    
+for heights_bed in heights_beds:
+    plt.plot(np.arange(len(planes)), [np.median(heights_bed[:,i]) for i in range(len(planes))])
+plt.show()
