5e99c72609dd19ac76697fc5fb2994ffbb3d5159
Martijn <martijnbos1996@xs4all.nl>
2019-09-16 11:02:00 +0200

    Initmatch test

--- Ridge/Controller.py
+++ Ridge/Controller.py
@@ -4,7 +4,7 @@ import RIDGE
 import RECCM
 import gdal
 
-l             = 0
+l             = 1
 base          = METAA.SelectBase("/")
 files         = METAA.SelectFiles(base)
 baseR         = METAA.SelectBase(base)
@@ -19,7 +19,7 @@ Ridges0,gt0F,fx0F,fy0F,Contour0F                             = RIDGE.DemOpening(
 
 for i in range(0,len(files)):
     print("[IMAGE "+str(i+1)+"/"+str(len(files))+"]")
-    gt1C,Img1C,ImgB1C,MaskB1C,fx1C,fy1C                     = METAA.OrthOpenin(psC,base)
+    gt1C,Img1C,ImgB1C,MaskB1C,fx1C,fy1C                     = METAA.OrthOpenin(psC,files[i])
     EdgeMap1C,OriMap1C,MaskMap1C,GradPoints1C               = CANNY.CannyPFree(psC,ImgB1C,MaskB1C)
     EdgesA1C,EdgesB1C,EdgesC1C,ChainsA1C                    = CANNY.CannyLines(psC,EdgeMap1C,OriMap1C,MaskMap1C,GradPoints1C)
     x_offset,y_offset,X1C,Y1C,X0C,Y0C,CV1                   = RECCM.SinglMatch(psC,w,md,EdgesA1C,gt1C,fx1C,fy1C,EdgesA0C,gt0C,fx0C,fy0C,MaskB0C)
@@ -27,4 +27,6 @@ for i in range(0,len(files)):
     gto1                                                    = gdal.Open(files[i]).GetGeoTransform()
     X1Oa,Y1Oa,Lon0Oa,Lat0Oa,X1Fa,Y1Fa,X0Fa,Y0Fa,CVa,dx,dy   = RECCM.PatchMatch(psF,w,md,Ridges1,gt1F,fx1F,fy1F,Ridges0,gt0F,fx0F,fy0F,Contour0F,x_offset,y_offset,CV1)
     X1Ob,Y1Ob,Lon0Ob,Lat0Ob,X1Fb,Y1Fb,X0Fb,Y0Fb,CVb,gcplist = RECCM.RemOutlier(X1Oa,Y1Oa,Lon0Oa,Lat0Oa,X1Fa,Y1Fa,X0Fa,Y0Fa,CVa,dx,dy,gt1F,gto1)
+    
+    METAA.CreateFigs(l,i,files,psC,psF,w,Img0C,Img1C,ImgB0C,ImgB1C,EdgesA0C,EdgesB0C,EdgesC0C,EdgesA1C,EdgesB1C,EdgesC1C,X0C,Y0C,X1C,Y1C,X0Fa,Y0Fa,X1Fa,Y1Fa,X0Fb,Y0Fb,X1Fb,Y1Fb,CVb,Ridges0,Ridges1)
     RECCM.Georegistr(i,files,gcplist)
