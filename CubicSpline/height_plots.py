0e35a4b412d1d38808eda41bf8f5c56b89976163
wroel <wytze.roel@gmail.com>
2019-09-18 11:23:10 +0200

    Height plot for straightish polygons

--- CubicSpline/height_plots.py
+++ CubicSpline/height_plots.py
@@ -88,11 +88,9 @@ for bed in height_beds:
     if np.mean(bed) < min_mean:
         min_mean = np.mean(bed)
 
-
 spindex = Index(bbox=(np.amin(np.array(plants)[:,0]), np.amin(np.array(plants)[:,1]), np.amax(np.array(plants)[:,0]), np.amax(np.array(plants)[:,1])))
 for i,plant in enumerate(plants):
     spindex.insert({'obj': plant, 'index': i}, bbox=(plant[0], plant[1], plant[0], plant[1]))
-
     
 fig = plt.figure()
 for i, bed in enumerate(beds):
@@ -121,3 +119,64 @@ for k in trange(len(beds), desc="driehoeken", position=0):
         for i,s in enumerate(tri.simplices):
             if tri_values[i]:
                 plt.fill(tri.points[s][:,0], tri.points[s][:,1], c=((np.mean(tri_values[i]) - min_mean)/(max_mean-min_mean), 0, 0))
+                
+#%%
+from shapely.geometry import LineString, LinearRing
+allpolys = []
+for bed in beds:
+    if bed.exterior:
+        points = list(bed.exterior.coords)
+        
+        for i in range(len(points) -2, 0, -1):
+            if abs(np.arctan((points[i][1] - points[i - 1][1])/(points[i][0] - points[i - 1][0])) - np.arctan((points[i + 1][1] - points[i][1])/(points[i + 1][0] - points[i][0]))) < np.pi/8:
+                del points[i]
+                
+        longest_line = None
+        longest_length = 0
+        for i in range(len(points)):
+            if Point(points[i]).distance(Point(points[(i + 1) % len(points)])) > longest_length:
+                longest_line = [points[i], points[(i + 1) % len(points)]]
+                longest_length = Point(points[i]).distance(Point(points[(i + 1) % len(points)]))
+                
+        ret = []
+        n = 20
+        for i in range(1, n + 1):
+            point_to_add = [(i*longest_line[0][0] + (n + 1 -i) * longest_line[1][0])*(1/(n+1)), (i*longest_line[0][1] + (n + 1 -i) * longest_line[1][1])*(1/(n+1))]
+            ret.append(point_to_add)
+            
+        slope = np.arctan((longest_line[1][1] - longest_line[0][1])/(longest_line[1][0] - longest_line[0][0])) + np.pi/2
+        ints = [LineString([(p[0]- 0.01*np.cos(slope),p[1] - 0.01*np.sin(slope)), (p[0] + 0.01*np.cos(slope),p[1] + 0.01*np.sin(slope))]).intersection(Polygon(points).exterior)[1] for p in ret]
+        
+        side1 = []
+        side2 = []
+        for p in points:
+            if LinearRing([(p[0], p[1]), ints[0].coords[0], ints[0].coords[1]]).is_ccw:
+                side1.append(p)
+            else:
+                side2.append(p)
+                
+        polys = []
+        for i in range(len(ints)-1):
+            polys.append(Polygon([ints[i].coords[0], ints[i].coords[1], ints[i+1].coords[1], ints[i+1].coords[0]]))
+        
+        polyunion = polys[0]
+        for i in range(1, len(polys)):
+            polyunion = polyunion.union(polys[i])
+            
+        diff_polys = sorted(Polygon(points).difference(polyunion), key=lambda p : p.area, reverse=True)
+        polys.append(diff_polys[0])
+        polys.append(diff_polys[1])
+        allpolys += polys
+
+poly_values = [[] for i in range(len(allpolys))]
+for i, poly in enumerate(allpolys):
+    for plant in spindex.intersect(poly.bounds):
+        if Point(plant['obj']).within(poly):
+            poly_values[i].append(heights[plant['index']][1] - heights[plant['index']][0])
+            
+min_mean = min([np.mean(v) if v else 100 for v in poly_values])
+max_mean = max([np.mean(v) if v else -100 for v in poly_values])
+
+for i, poly in enumerate(allpolys):
+    if poly.values[i]:
+        plt.fill(*poly.exterior.xy, c=((np.mean(poly_values[i]) - min_mean)/(max_mean-min_mean), 0, 0))
