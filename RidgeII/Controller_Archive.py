86a7734548d2a9b67c62b3b4b12c0291fa59a30e
eric <ericverhoeff91@hotmail.com>
2019-09-27 14:34:03 +0200

    esink

--- RidgeII/Controller_Archive.py
+++ RidgeII/Controller_Archive.py
@@ -15,9 +15,8 @@ if __name__ == '__main__':
         plist,gt1F,fx1F,fy1F,MaskB1F,Edges1F      = METAA.DemOpening(plist,path[i],Img1C)
     
         plist,x_off,y_off,x0,y0,xog,yog,x1,y1,CV1                                           = RECCM.SinglMatch(plist,Edges1C,gt1C,fx1C,fy1C,Edges0C,gt0C,fx0C,fy0C,MaskB0C)
-        plist,pbar,inp,pool                                                                 = RECCM.OooneMatch(plist,Edges0F,Edges1F,MaskB0F,CV1,gt0F,gt1F,fx0F,fy0F,fx1F,fy1F,x_off,y_off)       
-        plist,results                                                                       = RECCM.TwoooMatch(plist,pbar,inp,pool)
-        plist,origin_x,origin_y,target_lon,target_lat,x0,y0,xog,yog,xof,yof,x1,y1,CVa,dx,dy = RECCM.ThreeMatch(plist,pbar,results,Edges0F,Edges1F)
+        plist,pbar,inp,pool                                                                 = RECCM.OooneMatch(plist,Edges0F,Edges1F,MaskB0F,CV1,gt0F,gt1F,fx0F,fy0F,fx1F,fy1F,x_off,y_off)                                                             
+        plist,origin_x,origin_y,target_lon,target_lat,x0,y0,xog,yog,xof,yof,x1,y1,CVa,dx,dy = RECCM.ThreeMatch(plist,pbar,pool,inp,Edges0F,Edges1F)
         plist,origin_x,origin_y,target_lon,target_lat,x0,y0,x1,y1,CVa,gcplist1,gcplist2     = RECCM.RemOutSlop(plist,origin_x,origin_y,target_lon,target_lat,x0,y0,x1,y1,CVa,dx,dy,gt1F,path,i)
         plist                                                                               = METAA.CapFigures(i,path,plist)
         RECCM.Georegistr(i,path,gcplist1,gcplist2)
